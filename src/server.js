8b07acdb177eb675bc3c9eb7a257bae9135b0311
lijieniu <niulijie19890522@163.com>
2022-09-29 15:18:30 +0800

    server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import Koa from "koa";
 const app = new Koa();
+import fs from "fs";
 // 处理跨域
 app.use(async (ctx, next) => {
+  console.log(ctx);
   ctx.set("Access-Control-Allow-Origin", "*");
   ctx.set("Access-Control-Allow-Methods", "GET");
   ctx.set("Access-Control-Allow-Headers", "x-requested-with");
@@ -9,6 +11,14 @@ app.use(async (ctx, next) => {
     ctx.body = 200;
   } else {
     await next();
+  }
+});
+app.use(async (ctx, next) => {
+  if (ctx.request.url.endsWith(".html")) {
+    ctx.type = "html";
+    ctx.body = fs.readFileSync("." + ctx.request.url);
+  } else if (ctx.request.url.startsWith("/static/icon")) {
+    ctx.body = fs.readFileSync("." + ctx.request.url);
   }
 });
 app.listen(8080);
